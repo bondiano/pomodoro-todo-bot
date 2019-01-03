8ad6d6dfb58ec1d006212587c700bcec6ff2c974
bondiano <batbondik0@gmail.com>
2019-01-03 17:23:39 +0700

    Fixes for run multiply pomodoro timer

--- app/src/handlers/actions/pomodoro.ts
+++ app/src/handlers/actions/pomodoro.ts
@@ -6,7 +6,7 @@ import { millisToMinutesAndSeconds } from '@/utils';
 
 const timerPeriods = [25 * 60 * 1000, 5 * 60 * 1000, 25 * 60 * 1000, 5 * 60 * 1000, 25 * 60 * 1000, 15 * 60 * 1000];
 const INTERVAL_DURATION = 1000;
-const activeIntervals = {}
+const activeIntervals = {};
 
 const intervalHandler = async (ctx: IBotContext) => {
   if (ctx.session.currentPomodoro.currentTime <= 0) {
@@ -27,22 +27,26 @@ const intervalHandler = async (ctx: IBotContext) => {
   await ctx.editMessageText(millisToMinutesAndSeconds(ctx.session.currentPomodoro.currentTime), pausePomodoroExtra(ctx.i18n));
   ctx.session.currentPomodoro.currentTime -= INTERVAL_DURATION;
   await forceSaveSession(ctx);
-}
+};
 
 const startPomodoro = async (ctx: IBotContext) => {
+  if(ctx.session.currentPomodoro.currentTime) {
+    return;
+  }
+
   await ctx.reply(ctx.i18n.t('focusTime'));
   await ctx.editMessageReplyMarkup(pausePomodoroKeyboard(ctx.i18n));
   ctx.session.currentPomodoro.currentTime = timerPeriods[0];
   const sessionKey = getSessionKey(ctx);
   activeIntervals[sessionKey] = setInterval(intervalHandler, INTERVAL_DURATION, ctx);
-}
+};
 
 const continuePomodoro = async (ctx: IBotContext) => {
   await ctx.editMessageReplyMarkup(pausePomodoroKeyboard(ctx.i18n));
 
   const sessionKey = getSessionKey(ctx);
   activeIntervals[sessionKey] = setInterval(intervalHandler, INTERVAL_DURATION, ctx);
-}
+};
 
 const pausePomodoro = async (ctx: IBotContext) => {
   const sessionKey = getSessionKey(ctx);
@@ -50,7 +54,7 @@ const pausePomodoro = async (ctx: IBotContext) => {
   clearInterval(interval);
   activeIntervals[sessionKey] = null;
   await ctx.editMessageReplyMarkup(stopPomodoroKeyboard(ctx.i18n));
-}
+};
 
 const stopCurrentPomodoro = async (ctx: IBotContext) => {
   const sessionKey = getSessionKey(ctx);
@@ -61,18 +65,18 @@ const stopCurrentPomodoro = async (ctx: IBotContext) => {
   ctx.session.currentPomodoro.period = 0;
   ctx.session.currentPomodoro.currentTime = null;
   await ctx.editMessageReplyMarkup();
-}
+};
 
 const actionByType: { [key: string]: (ctx: IBotContext) => Promise<any> } = {
   start: startPomodoro,
   continue: continuePomodoro,
   pause: pausePomodoro,
   stop: stopCurrentPomodoro
-}
+};
 
 export const pomodoroActionHandler = async (ctx: IBotContext) => {
   if (!ctx.session.currentPomodoro) {
-    ctx.session.currentPomodoro = {}
+    ctx.session.currentPomodoro = {};
   }
 
   const [, type] = ctx.update.callback_query.data.split('/');
--- app/src/handlers/commands/reset-pomodoro.ts
+++ app/src/handlers/commands/reset-pomodoro.ts
@@ -3,4 +3,4 @@ import { IBotContext } from '@/typing';
 export const resetPomodoroHandler = (ctx: IBotContext) => {
   ctx.session.currentPomodoro.period = 0;
   ctx.session.currentPomodoro.currentTime = null;
-}
+};
